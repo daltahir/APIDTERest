22cb7d56e105b8be9f399f4a530552b784fdbdc9
Francisco Diaz <f.diaz@live.com>
2017-10-03 20:01:37 -0300

    Se modifican los metodos para que solo se realice la validacion de datos.

--- APIDTERest/Controllers/RecepcionDTEController.cs
+++ APIDTERest/Controllers/RecepcionDTEController.cs
@@ -10,119 +10,47 @@ namespace APIDTERest.Controllers
 {
     public class RecepcionDTEController : ApiController
     {
-
         public Retorno Post([FromBody] RecepcionDocumento value)
         {
-            string xmlString = "";
             Retorno result = new Retorno();
             bool resultado = true;
 
-            XmlDocument xdoc = new XmlDocument();
-            RecepcionDocumento documento = new RecepcionDocumento();
-            cabecera Cabecera = new cabecera();
-
-            XDocument xml = XDocument.Parse(xmlString);
-            XDocument xmlDTE = new XDocument();
-            XDocument xmlPDF = new XDocument();
-            XmlSchemaSet esquema = new XmlSchemaSet();
-            //byte[] bytesPDF;
-
-            esquema.Add("", @"C:\Users\fdiaz\Source\Repos\APIDTERest\APIDTERest\Resources\xsdDocumento.xsd");
-
-            if (Utils.validarEstructura(esquema, xml))
+            if (!Utils.validarRut(value.ID.RUT_EMISOR))
             {
-                xdoc.LoadXml(xmlString);
-                XmlNodeList RecepcionDocumento = xdoc.GetElementsByTagName("RecepcionDocumento");
-                XmlNodeList lista = ((XmlElement)RecepcionDocumento[0]).GetElementsByTagName("ID");
-
-                XmlNodeList rutEmisor;
-                XmlNodeList rutReceptor;
-                XmlNodeList tipoDTE;
-                XmlNodeList folio;
-                XmlNodeList DTE;
-                XmlNodeList PDF;
-                XmlNodeList URI;
-
-                foreach (XmlElement nodo in lista)
-                {
-                    rutEmisor = nodo.GetElementsByTagName("RUT_EMISOR");
-                    if (Utils.validarRut(rutEmisor[0].InnerText))
-                    {
-                        Cabecera.RUT_EMISOR = rutEmisor[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    rutReceptor = nodo.GetElementsByTagName("RUT_RECEPTOR");
-                    if (Utils.validarRut(rutReceptor[0].InnerText))
-                    {
-                        Cabecera.RUT_RECEPTOR = rutReceptor[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    tipoDTE = nodo.GetElementsByTagName("TIPO_DTE");
-                    if (Utils.validarNumero(tipoDTE[0].InnerText))
-                    {
-                        Cabecera.TIPO_DTE = tipoDTE[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    folio = nodo.GetElementsByTagName("FOLIO");
-                    if (Utils.validarNumero(folio[0].InnerText))
-                    {
-                        Cabecera.FOLIO = folio[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-                }
-
-                foreach (XmlElement nodo in RecepcionDocumento)
-                {
-
-                    URI = nodo.GetElementsByTagName("URI");
-                    if (URI[0].InnerText != "")
-                    {
-                        documento.URI = URI[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    DTE = nodo.GetElementsByTagName("XML_DTE");
-                    if (DTE[0].InnerText != "")
-                    {
-                        documento.XML_DTE = Utils.decoderBase64(DTE[0].InnerText);
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    PDF = nodo.GetElementsByTagName("PDF");
-                    if (PDF[0].InnerText != "")
-                    {
-                        //bytesPDF = Convert.FromBase64String(PDF[0].InnerText);
-                        documento.PDF = Convert.FromBase64String(PDF[0].InnerText);
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-                }
+                resultado = false;
             }
 
-            if(resultado)
+            if (!Utils.validarRut(value.ID.RUT_RECEPTOR))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarNumero(value.ID.TIPO_DTE))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarNumero(value.ID.FOLIO))
+            {
+                resultado = false;
+            }
+
+            if (value.URI == "")
+            {
+                resultado = false;
+            }
+
+            if (value.XML_DTE == "")
+            {
+                resultado = false;
+            }
+
+            if (value.PDF != null || value.PDF.Length != 0)
+            {
+                resultado = true;
+            }
+
+            if (resultado)
             {
                 result.Cod_Respuesta = "1";
                 result.Desc_Respuesta = "documento  recepcionado";
@@ -143,13 +71,13 @@ namespace APIDTERest.Controllers
             //    result.Cod_Respuesta = "0";
             //    result.Desc_Respuesta = "documento no recepcionado";
             //}
-            
+
             /*
             string result = "<Retorno>"  +
                             "<Cod_Respuesta>0</Cod_Respuesta >" +
                             "<Desc_Respuesta>Documento no Registrado</Desc_Respuesta>" +
                             "</Retorno> ";*/
-                            
+
             //result = "<Retorno>" +
             //            "<Cod_Respuesta>1</Cod_Respuesta >" +
             //            "<Desc_Respuesta>Documento Registrado</Desc_Respuesta>" +
--- APIDTERest/Controllers/RecepcionEventoController.cs
+++ APIDTERest/Controllers/RecepcionEventoController.cs
@@ -20,139 +20,63 @@ namespace APIDTERest.Controllers
         /// </summary>
         /// <param name="value"></param>
         /// <returns>xml</returns>
+        /// 
         public Retorno Post([FromBody]Evento value)
         {
-            string xmlString = "";
-            Retorno result =new Retorno();
+            Retorno result = new Retorno();
             bool resultado = true;
-            XDocument xml = XDocument.Parse(xmlString);
-            XmlSchemaSet esquema = new XmlSchemaSet();
-            //Evento eventos = new Evento();
-            esquema.Add("", @"C:\Users\fdiaz\Source\Repos\APIDTERest\APIDTERest\Resources\xsdEventos.xsd");
-
-            if (Utils.validarEstructura(esquema, xml))
+            
+            if (value.TipoEvento == "")
             {
-                XmlDocument xdoc = new XmlDocument();
-
-                xdoc.LoadXml(xmlString);
-                XmlNodeList nodoEventos = xdoc.GetElementsByTagName("Evento");
-
-                XmlNodeList tipoEvento;
-                XmlNodeList rutEmisor;
-                XmlNodeList rutReceptor;
-                XmlNodeList tipoDTE;
-                XmlNodeList folio;
-                XmlNodeList fechaEmision;
-                XmlNodeList fechaEvento;
-                XmlNodeList uri;
-                XmlNodeList descripcion_evento;
-                XmlNodeList observacion;
-
-                foreach (XmlElement nodo in nodoEventos)
-                {
-                    tipoEvento = nodo.GetElementsByTagName("TipoEvento");
-                    if (tipoEvento[0].InnerText != "")
-                    {
-                        value.TipoEvento = tipoEvento[0].InnerText;                        
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    rutEmisor = nodo.GetElementsByTagName("RutEmisor");
-                    if (Utils.validarRut(rutEmisor[0].InnerText))
-                    {
-                        value.RutEmisor = rutEmisor[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    rutReceptor = nodo.GetElementsByTagName("RutReceptor");
-                    if (Utils.validarRut(rutReceptor[0].InnerText))
-                    {
-                        value.RutReceptor = rutReceptor[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    tipoDTE = nodo.GetElementsByTagName("TipoDTE");
-                    if (Utils.validarNumero(tipoDTE[0].InnerText))
-                    {
-                        value.TipoDTE = Convert.ToInt32(tipoDTE[0].InnerText);
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    folio = nodo.GetElementsByTagName("Folio");
-                    if (Utils.validarNumero(folio[0].InnerText))
-                    {
-                        value.Folio = Convert.ToInt64(folio[0].InnerText);
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    fechaEmision = nodo.GetElementsByTagName("FechaEmision");
-                    if (Utils.validarFecha(fechaEmision[0].InnerText))
-                    {
-                        value.FechaEmision = Convert.ToDateTime(fechaEmision[0].InnerText);
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    fechaEvento = nodo.GetElementsByTagName("FechaEvento");
-                    if (Utils.validarFecha(fechaEvento[0].InnerText))
-                    {
-                        value.FechaEvento = Convert.ToDateTime(fechaEvento[0].InnerText);
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    uri = nodo.GetElementsByTagName("URI");
-                    if (uri[0].InnerText != "")
-                    {
-                        value.Uri = uri[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    descripcion_evento = nodo.GetElementsByTagName("Descripcion_Evento");
-                    if (descripcion_evento[0].InnerText != "")
-                    {
-                        value.DescripcionEvento = descripcion_evento[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-
-                    observacion = nodo.GetElementsByTagName("Observacion");
-                    if (observacion[0].InnerText != "")
-                    {
-                        value.Observacion = observacion[0].InnerText;
-                    }
-                    else
-                    {
-                        resultado = false;
-                    }
-                }
+                resultado = false;
             }
 
-            if(resultado)
+            if (!Utils.validarRut(value.RutEmisor))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarRut(value.RutReceptor))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarNumero(value.TipoDTE.ToString()))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarNumero(value.Folio.ToString()))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarFecha(value.FechaEmision.ToString()))
+            {
+                resultado = false;
+            }
+
+            if (!Utils.validarFecha(value.FechaEvento.ToString()))
+            {
+                resultado = false;
+            }
+
+            if (value.Uri == "")
+            {
+                resultado = false;
+            }
+
+            if (value.DescripcionEvento == "")
+            {
+                resultado = false;
+            }
+
+            if (value.Observacion == "")
+            {
+                resultado = false;
+            }
+
+            if (resultado)
             {
                 result.Cod_Respuesta = "1";
                 result.Desc_Respuesta = "Evento Agregado";
